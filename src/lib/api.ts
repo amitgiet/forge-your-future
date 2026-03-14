1ecc3fbe3ac838e197feda20bbe29eef88997b97
Bharat Kumar Khati <104752911+amitgiet@users.noreply.github.com>
2026-03-14 10:28:17 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = "https://backend-forge-neet.onrender.com";
-const baseURL = "http://100.56.20.190:5002";
+const baseURL = "https://backend-forge-neet.onrender.com";
+// const baseURL = "http://100.56.20.190:5002";
 //ne5sa
 // const baseURL = "http://localhost:5002";
 export const API_BASE_URL = baseURL;
